90dcfae1a1e1a7c0954811bbd49df334e4ba15eb
Camilo <camilo@tipddy.cl>
2021-07-20 14:16:04 -0400

    custom logo from wp theme

--- wp-content/themes/aquila/inc/classes/class-aquila-theme.php
+++ wp-content/themes/aquila/inc/classes/class-aquila-theme.php
@@ -22,10 +22,41 @@ class AQUILA_THEME {
         /**
          * Actions.
          */
+        add_action('after_setup_theme', [$this, 'setup_theme']);
         // add_action('wp_enqueue_scripts', [ $this, 'register_styles' ]);
         // add_action('wp_enqueue_scripts', [ $this, 'register_scripts' ]);  
     }
+    public function setup_theme() {
+
+		/**
+		 * Let WordPress manage the document title.
+		 * By adding theme support, we declare that this theme does not use a
+		 * hard-coded <title> tag in the document head, and expect WordPress to
+		 * provide it for us.
+		 */
+		add_theme_support( 'title-tag' );
+
+		/**
+		 * Custom logo.
+		 *
+		 * @see Adding custom logo
+		 * @link https://developer.wordpress.org/themes/functionality/custom-logo/#adding-custom-logo-support-to-your-theme
+		 */
+		add_theme_support(
+			'custom-logo',
+			[
+				'header-text' => [
+					'site-title',
+					'site-description',
+				],
+				'height'      => 100,
+				'width'       => 400,
+				'flex-height' => true,
+				'flex-width'  => true,
+			]
+		);
+
 
 }
-
+}
 ?>
